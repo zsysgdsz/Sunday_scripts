0682c2a4914bb1781ef82e19158b4cd70c50dcfc
Shuyuan Zhang <zsysgdsz@gmail.com>
2015-04-19 14:17:21 -0500

    Install and load dplyr package

--- Sunday_scripts.R
+++ Sunday_scripts.R
@@ -3,22 +3,12 @@
 # zsysgdsz@gmail.com
 # 4-19-15
 
+# Install dplyr package and dependencies
+install.packages("dplyr",dependencies=TRUE)
+
+#Load dplyr
+library("dplyr")
+
 #Read in data as tab-delimited
 gap.in <- read.table("output/combined_gapMinder.tsv", sep= "\t",header = TRUE)
-
-for(row.number in 1:10){
-  for (col.number in 1:5){
-    print(gap.in[row.number,col.number])
-  }
-}
-
-gap.in[1:10,1:5]
-
-add.me <-function(x,y){
-  (x + y)*8
-}
-add.me (3,4)
-
-
-
 
